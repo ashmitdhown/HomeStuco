43bcf78bf35927a584503bc8acd8873d045db751
Manavarya09 <smanavarya@gmail.com>
2025-09-24 12:57:52 +0400

    bruh

--- src/pages/Sparks.tsx
+++ src/pages/Sparks.tsx
@@ -11,9 +11,9 @@ const Sparks = () => {
         <section className="w-full h-[60vh] md:h-screen flex items-center justify-center relative overflow-hidden z-30 p-0 m-0">
           <div className="absolute inset-0 w-full h-full z-0">
             <picture>
-              <source media="(max-width: 640px)" srcSet="/assets/compressed/sparks.webp" />
+              <source media="(max-width: 640px)" srcSet="/assets/compressed/sparksww.png" />
               <img
-                src="/assets/compressed/sparks.webp"
+                src="/assets/compressed/sparksww.png"
                 alt="Sparks Background"
                 className="w-full h-full object-cover object-center"
                 style={{ minHeight: '60vh', maxHeight: '100vh' }}
